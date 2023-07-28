bb458eda7e32a4ffdec0f8f5862dceaf82793e91
Vaidas D <vaidasdbr@gmail.com>
2023-07-27 22:23:12 -0700

    testas_react

---
+++ r1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Princie from './Components/Princie';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello React</h1>
+        <Princie/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ r1/src/Components/Princie.jsx
@@ -0,0 +1,6 @@
+function Princie() {
+    return (
+        <h1>Hello!</h1>
+    );
+}
+export default Princie;
