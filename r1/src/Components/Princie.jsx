579d6c93ef42290294134c45f0a5e1ff367076d6
Vaidas D <vaidasdbr@gmail.com>
2023-07-30 11:07:09 -0700

    testas_react

--- r1/src/Components/Princie.jsx
+++ r1/src/Components/Princie.jsx
@@ -11,6 +11,14 @@ import Logo10 from './Img/logo10.png';
 import Logo11 from './Img/logo11.png';
 import Logo12 from './Img/logo12.png';
 import Logo13 from './Img/logo13.png';
+import Logo14 from './Img/logo14.png';
+import Logo15 from './Img/logo15.png';
+import Logo16 from './Img/logo16.png';
+import Logo17 from './Img/logo17.png';
+import Logo18 from './Img/logo18.png';
+import Logo19 from './Img/logo19.png';
+import Logo20 from './Img/logo20.png';
+import Logo21 from './Img/logo21.png';
 
 function Princie() {
     return (
@@ -27,7 +35,16 @@ function Princie() {
                         <p className="text2">@Maxie Mon Wachid</p>
                         <select className="btn-style6" name="" id=""></select>
                     </div>
-                    <p>Welcome back, Maxie</p>
+                    <p className="margin3">Welcome back, Maxie</p>
+                    <button className="btn-style7">
+                        <span>Download Report</span>
+                    </button>
+                    <div className="btn-style8">
+                        <img className="image5" src={Logo14} alt="Logo" />
+                        <p className="text3">This Month</p>
+                        <select className="btn-style6" name="" id=""></select>
+                    </div>
+                    <p className="text4">Maximize product sales and store management in order to get the best results.</p>
                 </div>
                 <div className="item2">
                     <div className="margin">
@@ -77,9 +94,94 @@ function Princie() {
                     </button>
                     </div>
                 </div>
-                <div className="item3">Menu2</div>
-                <div className="item4">Main</div>  
-                <div className="item5">Right</div>
+                <div className="item3">
+                    <div className="block2">
+                        <span className="text5">$</span>
+                        <span className="text3">Net Profit</span>
+                        <p className="text6">$</p>
+                        <p className="text7">302.1K</p>
+                        <p className="text8"><span className="color">&#8664; 29%</span> vs 3003k last year</p>
+                    </div>
+                    <div className="block2">
+                        <div className="block3">1</div>
+                        <img className="image6" src={Logo15} alt="Logo"/>
+                        <span className="text3">Store</span>
+                        <p className="text9">$</p>
+                        <p className="text7">12,900</p>
+                        <p className="text8"><span className="color2">&#8663; &#9840;29%</span> vs 1030 last year</p>
+                    </div>
+                    <div className="block2">
+                        <div className="block3">1</div>
+                        <img className="image6" src={Logo16} alt="Logo"/>
+                        <span className="text3">Product</span>
+                        <p className="text9">$</p>
+                        <p className="text7">390,040</p>
+                        <p className="text8"><span className="color2">&#8663; 41%</span> vs 320.583 last year</p>
+                    </div>
+                    <div className="block2">
+                        <div className="block3">1</div>
+                        <img className="image6" src={Logo17} alt="Logo"/>
+                        <span className="text3">Visitor</span>
+                        <p className="text9">$</p>
+                        <p className="text7">3.1M</p>
+                        <p className="text8"><span className="color">&#8664; 17%</span> vs 33M last year</p>
+                    </div>
+                </div>
+                <div className="item4">
+                    <div className="block4">
+                        <div className="block3">1</div>
+                        <img className="image6" src={Logo18} alt="Logo" />
+                        <span className="text3">Monthly Recurring Revenue</span>
+                        <span className="text10">...</span>
+                        <div className="block5">
+                            <p className="text3">Income</p>
+                            <p className="text11">$</p>
+                            <p className="text12">156,098.1</p>
+                            <p className="text8"><span className="color2">&#8663; 41%</span> vs 320.583 last year</p>
+                        </div>
+                        <div className="block5 margin4">
+                            <p className="text3">Spend</p>
+                            <p className="text13">$</p>
+                            <p className="text12">80,112.02</p>
+                            <p className="text8"><span className="color2">&#8663; 2%</span> vs 77.00002 last year</p>
+                        </div>
+                        <div className="block6 text3">
+                            <p>$100k</p>
+                            <p>$75k</p>
+                            <p>$50k</p>
+                            <p>$10k</p>
+                        </div>
+                        <div className="block7">
+                            <canvas className="border2"></canvas>
+                            <canvas className="border3 data"></canvas>
+                            <canvas className="border4 data2"></canvas>
+                            <canvas className="border3 data3"></canvas>
+                            <canvas className="border4 data4"></canvas>
+                            <canvas className="border3 data5"></canvas>
+                            <canvas className="border4 data6"></canvas>
+                            <canvas className="border3 data7"></canvas>
+                            <canvas className="border4 data8"></canvas>
+                            <canvas className="border3 data9"></canvas>
+                            <canvas className="border4 data10"></canvas>
+                        </div>
+                        <div className="text14">
+                            <span className="margin5">Dec 22</span>
+                            <span className="margin5">Jan 23</span>
+                            <span className="margin5">Feb 23</span>
+                            <span className="margin5">Mar 23</span>
+                            <span className="margin5">Apr 23</span>
+                        </div>
+                        <div className="image7">
+                        <img src={Logo19} alt="Logo" />
+                        </div>
+                        <div className="image8">
+                        <img src={Logo20} alt="Logo" />
+                        </div>
+                    </div>
+                </div>  
+                <div className="item5">
+                    <img className="image9" src={Logo21} alt="Logo" />
+                </div>
             </div>
         </>
     );
